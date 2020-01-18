b4a50881e6ebb5cdbc13e932c4398d020967bba2
peleg <peleghportal@gmail.com>
2020-01-18 14:04:32 +0200

    Added closing after opening

--- Vision.py
+++ Vision.py
@@ -2,8 +2,9 @@ from __future__ import print_function
 import cv2 as cv
 import argparse
 import numpy as np
+
 max_value = 255
-max_value_H = 360//2
+max_value_H = 360 // 2
 low_H = 23
 low_S = 164
 low_V = 0
@@ -19,85 +20,100 @@ low_V_name = 'Low V'
 high_H_name = 'High H'
 high_S_name = 'High S'
 high_V_name = 'High V'
+
+
 def on_low_H_thresh_trackbar(val):
     global low_H
     global high_H
     low_H = val
-    low_H = min(high_H-1, low_H)
+    low_H = min(high_H - 1, low_H)
     cv.setTrackbarPos(low_H_name, window_detection_name, low_H)
+
+
 def on_high_H_thresh_trackbar(val):
     global low_H
     global high_H
     high_H = val
-    high_H = max(high_H, low_H+1)
+    high_H = max(high_H, low_H + 1)
     cv.setTrackbarPos(high_H_name, window_detection_name, high_H)
+
+
 def on_low_S_thresh_trackbar(val):
     global low_S
     global high_S
     low_S = val
-    low_S = min(high_S-1, low_S)
+    low_S = min(high_S - 1, low_S)
     cv.setTrackbarPos(low_S_name, window_detection_name, low_S)
+
+
 def on_high_S_thresh_trackbar(val):
     global low_S
     global high_S
     high_S = val
-    high_S = max(high_S, low_S+1)
+    high_S = max(high_S, low_S + 1)
     cv.setTrackbarPos(high_S_name, window_detection_name, high_S)
+
+
 def on_low_V_thresh_trackbar(val):
     global low_V
     global high_V
     low_V = val
-    low_V = min(high_V-1, low_V)
+    low_V = min(high_V - 1, low_V)
     cv.setTrackbarPos(low_V_name, window_detection_name, low_V)
+
+
 def on_high_V_thresh_trackbar(val):
     global low_V
     global high_V
     high_V = val
-    high_V = max(high_V, low_V+1)
+    high_V = max(high_V, low_V + 1)
     cv.setTrackbarPos(high_V_name, window_detection_name, high_V)
+
+
 def set_kernel_value(val):
     global kernel_value
     kernel_value = val
 
-parser = argparse.ArgumentParser(description='Code for Thresholding Operations using inRange tutorial.')
-parser.add_argument('--camera', help='Camera divide number.', default=0, type=int)
-args = parser.parse_args()
-cap = cv.VideoCapture(args.camera)
-cv.namedWindow(window_capture_name)
-cv.namedWindow(window_detection_name)
 
-cv.createTrackbar("kernel", window_detection_name , 0, 50, set_kernel_value)
-cv.createTrackbar(low_H_name, window_detection_name , low_H, max_value_H, on_low_H_thresh_trackbar)
-cv.createTrackbar(high_H_name, window_detection_name , high_H, max_value_H, on_high_H_thresh_trackbar)
-cv.createTrackbar(low_S_name, window_detection_name , low_S, max_value, on_low_S_thresh_trackbar)
-cv.createTrackbar(high_S_name, window_detection_name , high_S, max_value, on_high_S_thresh_trackbar)
-cv.createTrackbar(low_V_name, window_detection_name , low_V, max_value, on_low_V_thresh_trackbar)
-cv.createTrackbar(high_V_name, window_detection_name , high_V, max_value, on_high_V_thresh_trackbar)
+cap = cv.VideoCapture(0)
+cv.namedWindow(window_capture_name, cv.WINDOW_NORMAL)
+cv.namedWindow(window_detection_name, cv.WINDOW_NORMAL)
+
+cv.createTrackbar("kernel", window_detection_name, 0, 50, set_kernel_value)
+cv.createTrackbar(low_H_name, window_detection_name, low_H, max_value_H, on_low_H_thresh_trackbar)
+cv.createTrackbar(high_H_name, window_detection_name, high_H, max_value_H, on_high_H_thresh_trackbar)
+cv.createTrackbar(low_S_name, window_detection_name, low_S, max_value, on_low_S_thresh_trackbar)
+cv.createTrackbar(high_S_name, window_detection_name, high_S, max_value, on_high_S_thresh_trackbar)
+cv.createTrackbar(low_V_name, window_detection_name, low_V, max_value, on_low_V_thresh_trackbar)
+cv.createTrackbar(high_V_name, window_detection_name, high_V, max_value, on_high_V_thresh_trackbar)
 
 while True:
-    
-    ret, frame = cap.read()
+
+    res, frame = cap.read()
     if frame is None:
         break
     frame_HSV = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
     frame_threshold = cv.inRange(frame_HSV, (low_H, low_S, low_V), (high_H, high_S, high_V))
-    
-    print(kernel_value)
-    kernel = np.ones((kernel_value,kernel_value),np.uint8)
-    #erosion = cv.erode(frame_threshold,kernel,iterations = 1)
-    #dilation = cv.dilate(erosion,kernel,iterations = 1)
-    opening = cv.morphologyEx(frame_threshold, cv.MORPH_OPEN, kernel)
 
-    contours,hierarchy = cv.findContours(opening, 1, 2)
+    kernel = np.ones((kernel_value, kernel_value), np.uint8)
+    # erosion = cv.erode(frame_threshold,kernel,iterations = 1)
+    # dilation = cv.dilate(erosion,kernel,iterations = 1)
+    opening = cv.morphologyEx(frame_threshold, cv.MORPH_OPEN, kernel)
+    closing = cv.morphologyEx(opening, cv.MORPH_CLOSE, kernel)
+
+    contours, hierarchy = cv.findContours(closing, 1, 2)
+
     if len(contours) > 0:
         for cnt in contours:
-            x,y,w,h = cv.boundingRect(cnt)
-            cv.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+            x, y, w, h = cv.boundingRect(cnt)
+            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            midX = int((x+x+w)/2)
+            midY = int((y+y+h)/2)
+            cv.circle(frame, (midX, midY), 2, (255, 255, 255), -1)
 
-    
     cv.imshow(window_capture_name, frame)
     cv.imshow(window_detection_name, frame_threshold)
-    
+
     key = cv.waitKey(30)
     if key == ord('q') or key == 27:
         break
