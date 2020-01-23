1339af651ef2f1fe76ae417729f6435747c5348c
TomerMir <mtomer@gmail.com>
2020-01-23 18:18:06 +0200

    Cleaned the code, almost ready...

--- Powercell_Vision.py
+++ Powercell_Vision.py
@@ -6,29 +6,39 @@ import math
 import imutils
 
 KNOWN_WIDTH = 18
+
 max_value = 255
-max_value_H = 360 // 2
-low_H = 20
+max_value_H = 180
+
+low_H = 22
 low_S = 115
 low_V = 80
+
 high_H = 38
 high_S = 242
 high_V = 255
+
 kernel_value = 15
 kernel_value_name = "Kernel"
+
 focal_length = 801
 focal_length_name = "Focal length"
+
 window_capture_name = 'Video Capture'
 window_detection_name = 'Object Detection'
+
 low_H_name = 'Low H'
 low_S_name = 'Low S'
 low_V_name = 'Low V'
 high_H_name = 'High H'
 high_S_name = 'High S'
 high_V_name = 'High V'
+
 image_mid_x = 640
 image_mid_y = 360
+
 camera_fov = 50
+
 image_total_pixels = 1280
 
 def on_low_H_thresh_trackbar(val):
@@ -95,7 +105,6 @@ cap = cv.VideoCapture(0)
 cv.namedWindow(window_capture_name, cv.WINDOW_NORMAL)
 cv.namedWindow(window_detection_name, cv.WINDOW_NORMAL)
 
-
 cv.createTrackbar(focal_length_name, window_detection_name, focal_length, 1500, set_focal_length)
 cv.createTrackbar(kernel_value_name, window_detection_name, kernel_value, 50, set_kernel_value)
 cv.createTrackbar(low_H_name, window_detection_name, low_H, max_value_H, on_low_H_thresh_trackbar)
@@ -104,7 +113,6 @@ cv.createTrackbar(low_S_name, window_detection_name, low_S, max_value, on_low_S_
 cv.createTrackbar(high_S_name, window_detection_name, high_S, max_value, on_high_S_thresh_trackbar)
 cv.createTrackbar(low_V_name, window_detection_name, low_V, max_value, on_low_V_thresh_trackbar)
 cv.createTrackbar(high_V_name, window_detection_name, high_V, max_value, on_high_V_thresh_trackbar)
-
 
 def get_width(cntr):
     marker = cv.minAreaRect(cntr)
@@ -127,8 +135,6 @@ def distance_to_camera(knownWidth, focalLength, perWidth):
 while True:
     res, frame = cap.read()
 
-    #frame = cv.imread("powercell.jpg")
-
     if frame is None:
         break
 
@@ -143,33 +149,28 @@ while True:
     contours, hierarchy = cv.findContours(closing, cv.RETR_EXTERNAL, 2)
 
     if len(contours) > 0:
-        for cnt in contours:
+        biggest_contour = max(contours, key = cv.contourArea)
 
-            contour_width = get_width(cnt)
-            if contour_width == 0:
-                break
+        contour_width = get_width(biggest_contour)
+        if contour_width == 0:
+            break
 
-            #peri = cv.arcLength(cnt, True)
-            #approx = cv.approxPolyDP(cnt, 0.01 * peri, True)
-            #cv.drawContours(frame, [approx], -1, (0, 0, 0), 5)
+        distance = distance_to_camera(KNOWN_WIDTH, focal_length, contour_width)
 
-            distance = distance_to_camera(KNOWN_WIDTH, focal_length, contour_width)
+        #x, y, w, h = cv.boundingRect(biggest_contour)
+        #cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 7)
 
-            x, y, w, h = cv.boundingRect(cnt)
+        center, radius = cv.minEnclosingCircle(biggest_contour)   
 
-            cv.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 7)
+        center = covert_center_touple_to_int(center)     
 
-            center, radius = cv.minEnclosingCircle(cnt)   
+        cv.circle(frame, center, int(radius) ,(0, 255, 0), 5)
 
-            center = covert_center_touple_to_int(center)     
+        distance_between_the_ball_and_the_center_of_the_image = image_mid_x - center[0]
 
-            cv.circle(frame, center, int(radius) ,(0, 255, 0), 5)
+        angle = (distance_between_the_ball_and_the_center_of_the_image * camera_fov)/ image_total_pixels
 
-            distance_between_the_ball_and_the_center_of_the_image = image_mid_x - center[0]
-
-            angle = (distance_between_the_ball_and_the_center_of_the_image * camera_fov)/ image_total_pixels
-
-            cv.putText(frame, str(angle), (center), cv.FONT_HERSHEY_SIMPLEX, 2.0, (0, 0, 0), 3)
+        cv.putText(frame, str(angle), (center), cv.FONT_HERSHEY_SIMPLEX, 2.0, (0, 0, 0), 3)
 
     cv.imshow(window_capture_name, frame)
     cv.imshow(window_detection_name, closing)
